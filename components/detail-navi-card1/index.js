ca17a67703ee1269eb8e4abdc285ea47e81cebc1
lohvc <453111462@qq.com>
2019-12-20 18:09:14 +0800

    update

---
+++ components/detail-navi-card1/index.js
@@ -0,0 +1,27 @@
+// components/detail-navi-card/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    componentsPath: String,
+    title: String,
+    desc: String,
+    icon: String,
+    question:String
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
