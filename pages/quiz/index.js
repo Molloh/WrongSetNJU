c640b207bfe0431eced780d248d562e90febada1
Molloh <molloh1997@gmail.com>
2019-12-20 23:35:15 +0800

    update

--- pages/quiz/index.js
+++ pages/quiz/index.js
@@ -88,8 +88,7 @@ Page({
       url: 'quiz',
       success: (res) => {
         let hisArr = [];
-        console.log(res);
-        for(let x in res.data.questions) {
+        for (let x in res.data.quizzes) {
           let dis = x.scored ? "正确率：" + x.correct_num + "/" + x.total_num : "尚未批改";
           let tmp = {
             date: x.category,
@@ -99,7 +98,9 @@ Page({
           }
           hisArr.push(tmp);
         }
-        console.log(hisArr);
+        this.setData({
+          historyQuizzes: hisArr
+        })
       },
     });
     
